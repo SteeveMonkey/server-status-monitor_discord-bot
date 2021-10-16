fbcccafd6ef7804fe888f3012c359c8e982f04f6
SteeveMonkey <steevemonkey@gmail.com>
2021-10-15 21:03:07 -0400

    Modified bot.js to recieve slash commands instead of recieving and preparing plain text

--- bot.js
+++ bot.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const auth = require('./auth.json');
-const { prefix } = require('./config.json');
 
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES] });
 client.commands = new Discord.Collection();
 client.pingTypes = new Discord.Collection();
-const cooldowns = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const serverTypeFiles = fs.readdirSync('./ping_type').filter(file => file.endsWith('.js'));
@@ -27,65 +25,35 @@ client.once('ready', () => {
 });
 
 // Command Handling
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
+	const command = client.commands.get(interaction.commandName);
 	if (!command) return;
 
-	if (command.guildOnly && message.channel.type !== 'text') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
-	}
-	else {
-		timestamps.set(message.author.id, now);
-		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-	}
-
-	if (message.channel.type !== 'dm') {
+	if (interaction.channel.type !== 'dm') {
 		try {
-			const permissions = message.guild.me.permissionsIn(message.channel);
+			const permissions = interaction.guild.me.permissionsIn(interaction.channel);
 
 			// message.author.send(`My permissions in the \`${message.channel.name}\` channel on \`${message.guild.name}\` are as follows\n\`\`\`${permissions.serialize()}\`\`\``);
 
 			if (!permissions.has('SEND_MESSAGES')) {
-				message.author.send(`I am unable to send a message in the \`${message.channel.name}\` channel on \`${message.guild.name}\`\nPlease make sure I have permission to send messages in that channel`);
+				interaction.author.send(`I am unable to send a message in the \`${interaction.channel.name}\` channel on \`${interaction.guild.name}\`\nPlease make sure I have permission to send messages in that channel`);
 				return;
 			}
 		}
 		catch (error) {
 			console.error(error);
-			message.author.send(`There was an error trying to determine permissions: \`${error.name}\` \`\`\`\n${error.message}\`\`\``);
+			interaction.author.send(`There was an error trying to determine permissions: \`${error.name}\` \`\`\`\n${error.message}\`\`\``);
 		}
 	}
 
 	try {
-		command.execute(message, args);
+		await command.execute(interaction);
 	}
 	catch (error) {
 		console.error(error);
-		message.channel.send(`There was an error trying to execute the \`${command.name}\` command: \`\`\`\n${error.message}\`\`\``);
+		await interaction.reply(`There was an error trying to execute the \`${command.data.name}\` command: \`\`\`\n${error.message}\`\`\``);
 	}
 });
 
