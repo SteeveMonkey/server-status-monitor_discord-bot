c35483c87f79f4f70d691bbd5211ca677c2c208c
SteeveMonkey <steevemonkey@gmail.com>
2022-02-22 13:20:30 -0500

    Simplified the options for the edit-embed command This command would now only open the editor for the given embed once complete

--- commands/edit-embed.js
+++ commands/edit-embed.js
@@ -1,30 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const ServerUtils = require('../server-utils');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('edit-embed')
-		.setDescription('Modifies a self-updating status message in the current channel')
+		.setDescription('Initiates GUI for modifing a self-updating status message in the current channel')
 		.addStringOption(option => option
 			.setName('embed-id')
 			.setDescription('Enter the ID of the status embed to modify')
-			.setRequired(true))
-		.addStringOption(option => option
-			.setName('attribute')
-			.setDescription('Enter the name of the attribute to modify')
-			.setRequired(false))
-		.addStringOption(option => option
-			.setName('value')
-			.setDescription('Enter the new value for the chosen attribute')
-			.setRequired(false)),
+			.setRequired(true)),
 	execute(interaction) {
 		const options = interaction.options;
 
 		// Handle args
 		const embedId = options.getString('embed-id');
-		const attributeId = options.getString('attribute');
-		const newValue = options.getString('value');
+
+		const embedData = ServerUtils.getStatusEmbedData(embedId, interaction.guild.id, interaction.channel.id);
 
 		// Edit self-updating server status embed
-		// TODO
+		// TODO: Present user with embed editor
+		interaction.reply({ content: 'WIP', ephemeral: true });
 	},
 };
